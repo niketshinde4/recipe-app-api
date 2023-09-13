f73b02c84cbdeb4e607bb43f20ab4e9ab9930bac
NiketShinde22@gmail.com <niket.shinde@coupa.com>
2023-09-13 13:51:22 +0530

    wait for DB

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,1 @@
-"""
-Tests
-"""
-from django.test import SimpleTestCase
 
-class CalcTests(SimpleTestCase):
-
-    def test_add_numbers(self):
-        res = calc.add(5, 6)
-        self.assertEqual(res, 11)
